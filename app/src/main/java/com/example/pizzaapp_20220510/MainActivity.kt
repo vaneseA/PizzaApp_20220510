2ffb99126c34626002c7d70813240881e948cc0f
DK <spol@naver.com>
2022-05-11 02:31:06 +0900

    프래그먼트 2개 생성

---
+++ app/src/main/java/com/example/pizzaapp_20220510/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pizzaapp_20220510
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValuese()
+    }
+
+    fun setupEvents() {
+    }
+
+    fun setValuese() {
+    }
+}
---
+++ app/src/main/java/com/example/pizzaapp_20220510/fragments/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.example.pizzaapp_20220510.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaapp_20220510.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_store, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+    
+}
